997047efcfdd5964f69f173ed49246e790ce73f4
Franck Bushbaum <franck.bushbaum@yahoo.com>
2021-09-18 14:40:15 -0500

    changed pic type from jpeg to jpg. GET route complete. Appended to DOM.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,45 @@
 import axios from 'axios';
 import React from 'react';
 import { useState, useEffect } from 'react';
-import Header from '../Header/Header.jsx'
 import './App.css';
 
+
+
 function App() {
+  const [pics, setPics] = useState([]); //These names I'm choosing are making me nervous.
+
+  const getPicsData = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery',
+    }).then((response) => {
+      // response.data is what we sent from the server
+      //axios has an object as a response with .data being
+      //what we sent from the server.
+      console.log('This is GET request data,',response.data);
+      setPics(response.data);
+    }).catch((error) => {
+      console.log(error);
+      alert('Error making GET request.');
+    })
+  }
+
+  useEffect(() => {
+    getPicsData();
+  }, []);
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-      <div class="container">
-        <div class="wrapper">
-          <img class="picture" src="images/goat_small.jpg"></img>
-          <h4 class="subtitle">Subtitle goes here</h4>
-          </div>
-        </div> 
-      </div>
-    );
+          <div class="container">
+              {pics.map(apic =>
+                (<div class="container"><img src={apic.path}/></div>))}
+          </div> 
+        </div>
+     );
 }
 
 export default App;
